1283215d01e57d7e57fe7cb28425a7124fe8bf26
Evans-mutuku <evansnyamai98@gmail.com>
2023-02-21 19:21:49 +0300

    added data and deleted to and from firestore

--- createTask.js
+++ createTask.js
@@ -1,12 +1,71 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBEC8nZF1gLS8mEJiNfpxyiLfC79JqXkA0",
-  authDomain: "task-man-c7972.firebaseapp.com",
-  projectId: "task-man-c7972",
-  storageBucket: "task-man-c7972.appspot.com",
-  messagingSenderId: "1047263220371",
-  appId: "1:1047263220371:web:1b0b88233ba16509e4b366",
-  measurementId: "G-9VR0K26NHG",
+  apiKey: "AIzaSyDepZoaHvu87ZkUv3nKLiqcMOIa1igo068",
+  authDomain: "plp-bootcamp.firebaseapp.com",
+  projectId: "plp-bootcamp",
+  storageBucket: "plp-bootcamp.appspot.com",
+  messagingSenderId: "572952880606",
+  appId: "1:572952880606:web:85064f68b8373aca40294f",
+  measurementId: "G-RW5BYY2CDG",
 };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
+var totalItems;
+var maxCode;
+var code;
+
+function storeTask(event) {
+  event.preventDefault();
+
+  var task = document.getElementById("task").value;
+  var desc = document.getElementById("desc").value;
+  document.getElementById("task").value = "";
+  document.getElementById("desc").value = "";
+
+  //  store data to firebase
+
+  firebase
+    .database()
+    .ref("TaskList/" + code)
+    .set({
+      task: task,
+      desc: desc,
+      status: "pending",
+    });
+
+  document.getElementById("tasks-header").insertAdjacentHTML(
+    "afterend",
+
+    `
+        <div class="Task-item" id="${code}">
+          <div class="data" id="${task}" >
+            <button class="done" id="done" onclick="changeStatus('${code}')" > <i class="far far-check-circle"></i> </button>
+            <h2 class="Task">${task}</h2>
+            <p class="desc">${desc}</p>
+            <p id="status"></p>
+          </div>
+          <hr>
+          <div class="buttons">
+            <button class=" button edit" id="editData" >EDIT TASK</button>
+            <button class=" button delete" id="deleteData" onclick="deleteData('${code}')"  >DELETE TASK</button>
+          </div>
+        </div>
+      `
+  );
+}
+
+function deleteData(code) {
+  firebase
+    .database()
+    .ref("TaskList/" + code)
+    .remove();
+  document.getElementById(code).remove();
+
+  firebase
+    .database()
+    .ref("TotalTasks")
+    .update({
+      totalItems: totalItems - 1,
+    });
+}
