7e10a4c4d33ddfad73427455aa01080ce20bc139
Evans-mutuku <evansnyamai98@gmail.com>
2023-02-21 12:04:32 +0300

    ready to start adding task

---
+++ createTask.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBEC8nZF1gLS8mEJiNfpxyiLfC79JqXkA0",
+  authDomain: "task-man-c7972.firebaseapp.com",
+  projectId: "task-man-c7972",
+  storageBucket: "task-man-c7972.appspot.com",
+  messagingSenderId: "1047263220371",
+  appId: "1:1047263220371:web:1b0b88233ba16509e4b366",
+  measurementId: "G-9VR0K26NHG",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
