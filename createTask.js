572285ccb4901077cfa59cc1ef6682a6ca608158
Evans-mutuku <evansnyamai98@gmail.com>
2023-02-22 19:54:05 +0300

    Done with Day Three with complete CRUD

--- createTask.js
+++ createTask.js
@@ -15,6 +15,61 @@ var totalItems;
 var maxCode;
 var code;
 
+//change the color and disabled attribute for edit button
+// show or hide the innerHTML of element with status id
+function changeStatus(code) {
+  var status;
+  firebase
+    .database()
+    .ref("TaskList/" + code)
+    .on("value", function (snapshot) {
+      status = snapshot.val().status;
+    });
+
+  if (status === "pending") {
+    firebase
+      .database()
+      .ref("TaskList/" + code)
+      .update({
+        status: "completed",
+      });
+    document.getElementById(code).querySelector("#done").style.color =
+      "#00b200";
+    document.getElementById(code).querySelector("#editbtn").disabled = true;
+    document
+      .getElementById(code)
+      .querySelector("#editbtn").style.backgroundColor =
+      "rgba(116, 116, 116, 0.671)";
+    document.getElementById(code).querySelector("#status").innerHTML = `
+            <i class="far fa-check-circle"></i> Completed
+            `;
+  } else {
+    firebase
+      .database()
+      .ref("TaskList/" + code)
+      .update({
+        status: "pending",
+      });
+    document.getElementById(code).querySelector("#done").style.color = "gray";
+    document.getElementById(code).querySelector("#editbtn").disabled = false;
+    if (
+      document.getElementById(code).querySelector("#editbtn").style
+        .removeProperty
+    ) {
+      document
+        .getElementById(code)
+        .querySelector("#editbtn")
+        .style.removeProperty("background-color");
+    } else {
+      document
+        .getElementById(code)
+        .querySelector("#editbtn")
+        .style.removeAttribute("background-color");
+    }
+    document.getElementById(code).querySelector("#status").innerHTML = "";
+  }
+}
+
 function storeTask(event) {
   event.preventDefault();
 
@@ -135,6 +190,87 @@ function cancelUpdation() {
   document.getElementById("form-btns").innerHTML = `
   <button type="submit" class="button add" id = "addTask" >󠀫󠀫<i class="fas fa-plus"></i> ADD TASK</button>
   `;
+}
+
+var data;
+firebase
+  .database()
+  .ref("TaskList")
+  .on("value", function (snapshot) {
+    data = snapshot.val();
+  });
+
+// Show All Functionality
+function showAll() {
+  if (data === null && document.getElementById("info") == null) {
+    document.getElementById("tasks-header").insertAdjacentHTML(
+      "afterend",
+      `<div class="no-task-info" id = "info">
+            <i class="fas fa-info-circle"></i>
+            No pending tasks
+        </div>`
+    );
+  }
+
+  for (code in data) {
+    var code = code;
+    var task = data[code]["task"];
+    var desc = data[code]["desc"];
+    var status = data[code]["status"];
+
+    var color;
+    if (status === "pending") {
+      color = "gray";
+    } else {
+      color = "#00b200";
+    }
+  }
+  // Show the data in the body in form of card
+  document.getElementById("tasks-header").insertAdjacentHTML(
+    "afterend",
+    `<div class="Task-item" id="${code}">
+      <div class="data" id="${task}">
+          
+          <h2 class="Task">${task}</h2>
+          <p class="desc">${desc}</p>
+          <small id = "status"></small>
+      </div>
+      <hr>
+      <div class="buttons">
+          <button class="button edit" id="editbtn" onclick = "editData('${code}')"><i class="fas fa-edit"></i> EDIT TASK</button>
+          <button class="button delete" id="deletebtn" onclick = "deleteData('${code}')"><i class="fas fa-trash-alt"></i> DELETE TASK</button>
+      </div>
+      
+      </div>`
+  );
+
+  if (status === "pending") {
+    document.getElementById(code).querySelector("#editbtn").disabled = false;
+    if (
+      document.getElementById(code).querySelector("#editbtn").style
+        .removeProperty
+    ) {
+      document
+        .getElementById(code)
+        .querySelector("#editbtn")
+        .style.removeProperty("background-color");
+    } else {
+      document
+        .getElementById(code)
+        .querySelector("#editbtn")
+        .style.removeAttribute("background-color");
+    }
+    document.getElementById(code).querySelector("#status").innerHTML = "";
+  } else {
+    document.getElementById(code).querySelector("#editbtn").disabled = true;
+    document
+      .getElementById(code)
+      .querySelector("#editbtn").style.backgroundColor =
+      "rgba(116, 116, 116, 0.671)";
+    document.getElementById(code).querySelector("#status").innerHTML = `
+          <i class="far fa-check-circle"></i> Completed
+          `;
+  }
 }
 
 // Delete everything in our database
